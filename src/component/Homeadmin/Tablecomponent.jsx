2ea1d649b806b89b15110430f1b6e44f27243e43
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-13 22:53:59 +0700

    add  display no members

--- src/component/Homeadmin/Tablecomponent.jsx
+++ src/component/Homeadmin/Tablecomponent.jsx
@@ -12,19 +12,26 @@ const Tablecomponent = ({members ,deleteMember}) => {
       </tr>
     </thead>
     <tbody>
-     {members.length > 0  && 
-      members.map((element)=>{
-      return(
+      
+
+     {members.length > 0 
+     ?  members.map((element)=>{
+        return(
           <tr className=" text-center border-b-2 border-gray-300" key={element.id}>
             <td>{element.name}</td>
             <td>{element.lastname}</td>
             <td>{element.position}</td>
             <td><button className="btn bg-red-500 hover:bg-red-400 text-white  px-[1.5rem]" onClick={()=> {deleteMember(element.id)}}>Delete</button></td>
+          </tr>
+        )})
+     :  <tr>
+          <td colSpan={4} className=" text-red-600">No members</td>
         </tr>
-      )
-     })}
-     
-     
+    
+    } 
+         
+   
+
     </tbody>
   </table>
   )
