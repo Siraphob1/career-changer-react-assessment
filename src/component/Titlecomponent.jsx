86ec340873bf3df4fe8921597dbb4358a09c70d7
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-13 16:41:23 +0700

    create TiltleComponent

--- src/component/Titlecomponent.jsx
+++ src/component/Titlecomponent.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom"
 
 const Titlecomponent = ({title}) => {
   return (
-    <section className=" flex flex-col items-center w-full border border-black">
-       <section className=" text-[2rem] font-bold my-[2rem]">
-          <h2 className=" leading-[2rem]">Generation Thailand</h2>
-          <h2 className=" leading-[2rem]">{title}</h2>
+    <section className=" flex flex-col items-center w-full ">
+       <section className=" text-[2rem] font-bold my-[3rem] text-center">
+          <h2 className=" leading-[2.4rem]">Generation Thailand</h2>
+          <h2 className=" leading-[2.4rem]">{title}</h2>
        </section>
-        <section className=" text-[1.2rem] font-semibold">
-         <Link to="/homeuser" className="border border-black mr-[1rem]">User Home Sector</Link>
-         <Link to="/homeadmin" className="border border-black">Admin Home Sector</Link>
+        <section>
+         <Link to="/homeuser" className=" mr-[1rem] btn text-[1.1rem] font-semibold normal-case" >User Home Sector</Link>
+         <Link to="/homeadmin" className=" btn text-[1.1rem] font-semibold normal-case">Admin Home Sector</Link>
         </section>
     </section>
   )
