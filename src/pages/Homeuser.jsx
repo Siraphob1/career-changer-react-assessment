c6776bf9050cd4fa24c3617d83b4191a878f09c2
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-13 16:42:16 +0700

    add table

--- src/pages/Homeuser.jsx
+++ src/pages/Homeuser.jsx
@@ -1,14 +1,44 @@
+import { useContext } from "react"
 import Navbarcomponent from "../component/Navbarcomponent"
 import Titlecomponent from "../component/Titlecomponent"
+import DataContext from "../Data/DataContext"
 
 
 
 const Homeuser = () => {
   const title = 'Home - User Sector' 
+  const {members , setMembers} = useContext(DataContext)
+
   return (
-    <div>
+    <div data-theme="light" className=" min-h-screen">
       <Navbarcomponent/>
       <Titlecomponent title={title}/>
+      <div className="overflow-x-auto  mt-[5rem] flex justify-center">
+        <table className="table w-[900px] border border-black">
+          {/* head */}
+          <thead>
+            <tr className=" text-center">
+              <th className="font-bold">Name</th>
+              <th>LastName</th>
+              <th>Position</th>
+            </tr>
+          </thead>
+          <tbody>
+           {members.length > 0 &&
+           members.map((element)=>{
+            return(
+                <tr className=" text-center" key={element.id}>
+                  <td>{element.name}</td>
+                  <td>{element.lastname}</td>
+                  <td>{element.position}</td>
+              </tr>
+            )
+           })}
+           
+           
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
