72f2f80ef6c1066f47567b82cb59176fdee37418
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-13 12:09:41 +0700

    add nav link

--- src/component/Navbarcomponent.jsx
+++ src/component/Navbarcomponent.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"
+
 
 const Navbarcomponent = () => {
   return (
-    <div>Navbarcomponent</div>
+    <nav>
+      <ul className=" flex justify-end border border-black py-[1rem] px-[1rem] gap-x-[1rem]">
+        <li className=" text-[1.3rem]"><Link to="/">Home</Link></li>
+        <li className=" text-[1.3rem]"><Link to="/owner">Owner</Link></li>
+      </ul>
+    </nav>
   )
 }
 
