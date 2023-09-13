2d6081f75f3d1d2895ff6084d8796aa47792f092
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-13 16:39:56 +0700

    update link navbar

--- src/component/Navbarcomponent.jsx
+++ src/component/Navbarcomponent.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom"
 const Navbarcomponent = () => {
   return (
     <nav>
-      <ul className=" flex justify-end border border-black py-[1rem] px-[1rem] gap-x-[1rem]">
-        <li className=" text-[1.3rem]"><Link to="/">Home</Link></li>
-        <li className=" text-[1.3rem]"><Link to="/owner">Owner</Link></li>
+      <ul className=" flex justify-end border-b border-black py-[1rem] px-[3rem] gap-x-[3rem] text-[1.3rem] font-medium">
+        <li ><Link to="/">Home</Link></li>
+        <li ><Link to="/owner">Owner</Link></li>
       </ul>
     </nav>
   )
