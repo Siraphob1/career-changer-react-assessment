0552fe6d8cbe16492f60a08158c2ea22e92cc0cc
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-13 10:34:20 +0700

    add router path

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,37 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import './index.css'
+
+import Home from './pages/Home';
+import Homeuser from './pages/Homeuser';
+import Homeadmin from './pages/Homeadmin';
+import Owner from './pages/Owner';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,    
+  },
+  {
+    path: "/home-user",
+    element: <Homeuser />,    
+  },
+  {
+    path: "/home-admin",
+    element: <Homeadmin />,    
+  },
+  {
+    path: "/owner",
+    element: <Owner />,    
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={} />
+  <RouterProvider router={router} />
 )
+
+
+//  https://www.youtube.com/
+//https://www.youtube.com/watch?v=1QYBiNRu1ok&list=RD1QYBiNRu1ok&start_radio=1
