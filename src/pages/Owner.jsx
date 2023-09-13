3617ef7e9df4452366a1b9c2f767b2d3132a3ffe
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-13 16:42:31 +0700

    add theme

--- src/pages/Owner.jsx
+++ src/pages/Owner.jsx
@@ -3,7 +3,7 @@ import Navbarcomponent from "../component/Navbarcomponent"
 
 const Owner = () => {
   return (
-    <div>
+    <div data-theme="light" className=" min-h-screen">
       <Navbarcomponent/>
     </div>
   )
